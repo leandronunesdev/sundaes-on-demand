ae1c50a74bde3ab28b098e8413b458afd7752024
leandronunesdev <nunes.pessoal@gmail.com>
2023-04-05 18:29:31 -0300

    Code and test topping options images from server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: 'Cherries', imagetPath: '/images/cherries.png' },
+        { name: 'M&Ms', imagetPath: '/images/m-and-ms.png' },
+        { name: 'Hot fudge', imagetPath: '/images/hot-fudge.png' },
+      ])
+    );
+  }),
 ];
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -6,11 +6,28 @@ test('displays image for each scoop from the server', async () => {
   render(<Options optionType='scoops' />);
 
   // find images
-
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
-  // confim alt text of images
-  const altText = scoopImages.map((elelement) => elelement.alt);
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping from the server', async () => {
+  render(<Options optionType='toppings' />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
 });
