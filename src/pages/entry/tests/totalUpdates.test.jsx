d867233ca84bc0e9fd3f5a90b85c973d0ec58245
leandronunesdev <nunes.pessoal@gmail.com>
2023-04-06 16:20:04 -0300

    Current status

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
+
+test('update scoop subtotal when scoops change', async () => {
+  const user = userEvent.setup();
+  render(<Options optionType='scoops' />, { wrapper: OrderDetailsProvider });
+
+  // make sure total starts out at $0.00
+  const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  // update vanilla scoops to 1, and check subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '2');
+  expect(scoopsSubtotal).toHaveTextContent('4.00');
+
+  // update chocolate scoops to 2 and check subtotal
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, '0');
+  expect(scoopsSubtotal).toHaveTextContent('4.00');
+});
