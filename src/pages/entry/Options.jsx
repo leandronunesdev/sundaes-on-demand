a3c3d0063b719b17f953840184756714ad6eb78d
leandronunesdev <nunes.pessoal@gmail.com>
2023-04-10 14:05:06 -0300

    implements grand total tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Row from "react-bootstrap/Row";
-import ScoopOption from "./ScoopOption";
-import ToppingOption from "./ToppingOption";
-import AlertBanner from "../common/AlertBanner";
-import { pricePerItem } from "../../constants";
-import { formatCurrency } from "../../utilities";
-import { useOrderDetails } from "../../contexts/OrderDetails";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { formatCurrency } from '../../utilities';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -15,10 +15,17 @@ export default function Options({ optionType }) {
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
+    const controller = new AbortController();
     axios
-      .get(`http://localhost:3030/${optionType}`)
+      .get(`http://localhost:3030/${optionType}`, { signal: controller.signal })
       .then((response) => setItems(response.data))
-      .catch((error) => setError(true));
+      .catch((error) => {
+        if (error.name !== 'CanceledError') setError(true);
+      });
+
+    return () => {
+      controller.abort();
+    };
   }, [optionType]);
 
   if (error) {
@@ -26,7 +33,7 @@ export default function Options({ optionType }) {
     return <AlertBanner />;
   }
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
   const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import OrderEntry from '../OrderEntry';
 
 test('update scoop subtotal when scoops change', async () => {
   const user = userEvent.setup();
@@ -36,18 +37,96 @@ test('update toppings subtotal when toppings change', async () => {
   });
   expect(toppingsSubtotal).toHaveTextContent('0.00');
 
-  const cherriesInput = await screen.findByRole('checkbox', {
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
     name: 'Cherries',
   });
-  await user.click(cherriesInput);
+  await user.click(cherriesCheckbox);
   expect(toppingsSubtotal).toHaveTextContent('1.50');
 
-  const mAndMsInput = await screen.findByRole('checkbox', {
+  const mAndMsCheckbox = await screen.findByRole('checkbox', {
     name: 'M&Ms',
   });
-  await user.click(mAndMsInput);
+  await user.click(mAndMsCheckbox);
   expect(toppingsSubtotal).toHaveTextContent('3.0');
 
-  await user.click(cherriesInput);
+  await user.click(cherriesCheckbox);
   expect(toppingsSubtotal).toHaveTextContent('1.50');
+});
+
+describe('grand total', () => {
+  test('grand total starts at $0.00', () => {
+    const { unmount } = render(<OrderEntry />);
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+    expect(grandTotal).toHaveTextContent('0.00');
+
+    unmount();
+  });
+
+  test('grand total updates properly if scoop is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '1');
+    expect(grandTotal).toHaveTextContent('2.00');
+
+    const mAndMsCheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+    await user.click(mAndMsCheckbox);
+    expect(grandTotal).toHaveTextContent('3.50');
+  });
+
+  test('grand total updates properly if topping is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    const mAndMsCheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+    await user.click(mAndMsCheckbox);
+    expect(grandTotal).toHaveTextContent('1.50');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '1');
+    expect(grandTotal).toHaveTextContent('3.50');
+  });
+
+  test('grand total updates properly if item is removed', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    const mAndMsCheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+    await user.click(mAndMsCheckbox);
+    expect(grandTotal).toHaveTextContent('1.50');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '1');
+    expect(grandTotal).toHaveTextContent('3.50');
+
+    await user.click(mAndMsCheckbox);
+    expect(grandTotal).toHaveTextContent('2.00');
+  });
 });
